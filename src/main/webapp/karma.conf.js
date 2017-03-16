16c7811179f0834f74358c9a529e75a3971ef2ea
The_Bous <thebous1993@gmail.com>
2017-03-16 10:15:13 +0100

    karma import file

--- src/main/webapp/karma.conf.js
+++ src/main/webapp/karma.conf.js
@@ -15,7 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        ./
+        './bower_components/angular/angular.js',
+        './bower_components/angular-mocks/angular-mocks.js',
+        './services/**/*.js',
+        './units/**/*.js'
     ],
 
 
