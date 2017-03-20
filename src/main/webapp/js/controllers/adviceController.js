93ae0b38eb2deb6aff0485a10c85c085645375b9
Michele Falzone <michelefalzone@gmail.com>
2017-03-20 18:06:28 +0100

    Added change feedback in advice

--- src/main/webapp/js/controllers/adviceController.js
+++ src/main/webapp/js/controllers/adviceController.js
@@ -4,4 +4,9 @@ RoboAdviceApp.controller("AdviceController",function($scope,strategyService){
   $scope.currentStrategy = strategyService.getLastStrategy();
   $scope.currentStrategyTipology = "Custom";
 
+  $scope.changeStrategy = function(){
+    // the user wants to change the current strategy based on advice
+    alert("the user wants to change his life");
+  }
+
 });
