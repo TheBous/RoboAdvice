8b29d0788195196ea397d30aa7485ebbd0ccf87e
Michele Falzone <michelefalzone@gmail.com>
2017-03-22 17:13:56 +0100

    fixed realtime graph, added formatted minutes, and refresh logic

--- src/main/webapp/js/controllers/realtimeGraphController.js
+++ src/main/webapp/js/controllers/realtimeGraphController.js
@@ -13,23 +13,50 @@ RoboAdviceApp.controller("RealtimeGraphController",function($scope,strategyServi
   }
 
   // increment the data using forecast
-  $scope.incrementData = function(data){
-    let rnd = Math.floor((Math.random()*100)+1)
-    if(rnd % 2 == 0)rnd*=-1;
+  $scope.incrementData = function(forecastValue){
     let now = new Date();
+    let lastValue = $scope.lastStrategyAmounts[$scope.lastStrategyAmounts.length-1];
+    let pulse = 0;
+
+    if(forecastValue != null){
+      // get the increment for seconds
+      // we have 2 hours until the token expires, so we have 60*2*60 seconds = 7200 seconds
+      // let calc the pulse, it is the difference betwwen forecastValue and last value
+      // per second
+      pulse = (forecastValue-lastValue)/7;
+      //$log.debug("realtimeGraphCtrl| forecastValue: " + forecastValue);
+      //$log.debug("realtimeGraphCtrl| realtimeValue: " + lastValue);
+    }
 
     $scope.currentTime = now.getDateFormatted() + " " + now.getHours() + ":" + now.getMinutes();
-
-    // new value
-    let lastValue = $scope.lastStrategyAmounts[$scope.lastStrategyAmounts.length-1];
-
+    let newValue = 0;
+    if(pulse!=0){
+      // if there is some change in the value, set the new value
+      let rnd = Math.floor((Math.random()*pulse)+1);
+      newValue = lastValue + rnd;
+    }
     // add the new value
-    $scope.lastStrategyAmounts.push(lastValue + rnd);
-
-    // add the new date
-    let updateTime = new Date();
-    $scope.lastStrategyDates.push(now.getHours() + ":" + now.getMinutes());
+    $log.debug("realtimeGraphCtrl| pulse: " + pulse);
+    /*
+      MAGIC
+    */
+    if($scope.lastStrategyAmounts.length > 4){
+      let last_index = $scope.lastStrategyAmounts.length-1;
+      for(let index = 1; index<last_index; index++){
+        $scope.lastStrategyAmounts[index] = $scope.lastStrategyAmounts[index+1];
+        $scope.lastStrategyDates[index] = $scope.lastStrategyDates[index+1];
+      }
+      $scope.lastStrategyAmounts[last_index] = newValue;
+      $scope.lastStrategyDates[last_index] = now.getHoursFormatted() + ":" + now.getMinutesFormatted();
+    }else{
+      $scope.lastStrategyAmounts.push(newValue);
+      $scope.lastStrategyDates.push(now.getHoursFormatted() + ":" + now.getMinutesFormatted());
+    }
     saveRealtime();
+    /*
+      /MAGIC
+    */
+
   };
 
   // check if exists somwthing in the local storage
