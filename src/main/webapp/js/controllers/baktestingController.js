cc6dabcc5625d994ea4e304716d095bf9ed86027
The_Bous <thebous1993@gmail.com>
2017-03-23 12:30:35 +0100

    some modifies on backtesting

--- src/main/webapp/js/controllers/baktestingController.js
+++ src/main/webapp/js/controllers/baktestingController.js
@@ -4,7 +4,7 @@ RoboAdviceApp.controller("Backtesting", function ($scope, $log, portfolioService
     $scope.yAxis = [];
     $scope.user = userService;
 
-    //$scope.loadingInProgress = true;
+    $scope.loadingInProgress = true;
     // $log.debug($scope.yAxis);
 
     $scope.update = function(data) {
@@ -21,7 +21,7 @@ RoboAdviceApp.controller("Backtesting", function ($scope, $log, portfolioService
                 $log.debug($scope.loadingInProgress);
                 $log.debug("backtesting Controller | callback");
                 for(let i = 0;i<response.data.length;i++){
-                    $log.debug(response.data[i].date);
+                    //$log.debug(response.data[i].date);
                     formattedDateGraph[i] = response.data[i].date.dayOfMonth + "/" + response.data[i].date.monthValue + "/" + response.data[i].date.year;
                     formattedValueGraph[i] = response.data[i].totalAmount;
                     //$log.debug(formattedDateGraph[i]);
@@ -36,26 +36,10 @@ RoboAdviceApp.controller("Backtesting", function ($scope, $log, portfolioService
 
             }
             else{
-
-
-
                 $scope.loadingInProgress = false;
-
-
-
                 $log.debug(response.statusCode);
                 $log.debug("backtesting Controller | error on backtesting ");
             }
         });
-        /*.$promise.then(
-         function(response) {
-         if(response.statusCode == 0){
-         $log.debug("Backtesting controller | statusCode = 0");
-         $log.debug(response.data);
-         }
-         else{
-         $log.debug("Backtesting controller | error backtesting");
-         }
-         });*/
     }
 });
