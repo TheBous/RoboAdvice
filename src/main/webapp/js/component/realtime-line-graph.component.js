4e927174a5b93cb67815a0264c76dfc00bb89240
Michele Falzone <michelefalzone@gmail.com>
2017-03-24 16:28:03 +0100

    added static data in realtime

--- src/main/webapp/js/component/realtime-line-graph.component.js
+++ src/main/webapp/js/component/realtime-line-graph.component.js
@@ -7,51 +7,111 @@ RoboAdviceApp.component("realtimeLineGraph",{
         forecastValue: "@",   // forecast value to obtain
         stimatedAmount: "<"   // the stimated amount from the controller
     },
-    template: `
-    <div style="text-align:left;float:left" ng-if="$ctrl.stimatedAmount">
-      Stimated Amount: <b>{{$ctrl.stimatedAmount}}</b>
-    </div>
-    <div style="text-align:right">
-      {{$ctrl.currentTime}}
-    </div>
-    <br>
-    <div>
-      <canvas id="line" class="chart chart-line" chart-data="$ctrl.verticalAxis" chart-labels="$ctrl.horizzontalAxis" chart-options="options">
-      </canvas
-    </div>
-  `,
+    templateUrl: "../../html/realtimeGraph.html",
     controller: function($scope,$interval,$log){
         var $ctrl = this;
         var scope = $scope;
 
         this.$onInit = function(){
             let now = new Date();
+            $log.debug("realtimeLineGrap| initialized")
+            $log.debug(this.verticalAxis);
+            $log.debug(this.horizzontalAxis);
 
             $scope.options = {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:false
-//              min: minY
-//              max: maxY
-                        }
-                    }]
-                }
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero:false
+                  }
+                }]
+              }
             };
 
             $scope.getNewData = function(){
               $ctrl.currentTime = now.getDateFormatted() + " " + now.getHours() + ":" + now.getMinutes();
-              let $this = this;
-              $ctrl.incrementData({data:$ctrl.forecastValue})
+              //let $this = this;
+              //$ctrl.incrementData({data:$ctrl.forecastValue})
             };
 
             if(this.realtime == "true"){
                   $log.debug("realtime component| realtime is setted");
                 //each minute
                 //this.incrementData();
-                $interval($scope.getNewData,5000,0,true);
+                //$interval($scope.getNewData,5000,0,true);
             }
-        }
+
+            // realtime highchart
+            Highcharts.chart('rt', {
+                chart: {
+                    type: 'spline',
+                    animation: Highcharts.svg, // don't animate in old IE
+                    marginRight: 10,
+                    events: {
+                        load: function () {
+
+                            // set up the updating of the chart each second
+                            var series = this.series[0];
+
+                            setInterval(function(){
+                              $log.debug("Incrementing data for realtime graph");
+                              //let x = ,y = ;
+
+                              //series.push([x,y],true,true);
+                            }, 1000);
+
+                        }
+                    }
+                },
+                title: {
+                    text: ''
+                },
+                xAxis: {
+                    type: 'datetime',
+                    tickPixelInterval: 150
+                },
+                yAxis: {
+                    title: {
+                        text: 'Value'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                tooltip: {
+                    formatter: function () {
+                        return '<b>' + this.series.name + '</b><br/>' +
+                            Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                            Highcharts.numberFormat(this.y, 2);
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                exporting: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'Our data',
+                    data: (function () {
+                        // set the first data
+                        var time = (new Date()).getTime();
+                        var data = [];
+                        $ctrl.verticalAxis.forEach(function(element,$index){
+                          data.push({
+                            x: $ctrl.horizzontalAxis[$index],
+                            y: element
+                          });
+                        });
+                        return data;
+
+                    }())
+                }]
+            });
+
+        }// end onInit
 
     }
 });
--- src/main/webapp/js/controllers/realtimeGraphController.js
+++ src/main/webapp/js/controllers/realtimeGraphController.js
@@ -12,77 +12,29 @@ RoboAdviceApp.controller("RealtimeGraphController",function($scope,strategyServi
     $scope.lastStrategyDates = angular.fromJson(sessionStorage.realtimeDates);
   }
 
-  // increment the data using forecast
-  $scope.incrementData = function(forecastValue){
-    let now = new Date();
-    let lastValue = $scope.lastStrategyAmounts[$scope.lastStrategyAmounts.length-1];
-    let pulse = 0;
-
-    if(forecastValue != null){
-      // get the increment for seconds
-      // we have 2 hours until the token expires, so we have 60*2*60 seconds = 7200 seconds
-      // let calc the pulse, it is the difference betwwen forecastValue and last value
-      // per second
-      pulse = (forecastValue-lastValue)/7;
-      //$log.debug("realtimeGraphCtrl| forecastValue: " + forecastValue);
-      //$log.debug("realtimeGraphCtrl| realtimeValue: " + lastValue);
-    }
-
-    $scope.currentTime = now.getDateFormatted() + " <b>" + now.getHours() + ":" + now.getMinutes() + "</b>";
-    let newValue = 0;
-    if(pulse!=0){
-      // if there is some change in the value, set the new value
-      let rnd = Math.floor((Math.random()*pulse)+1);
-      newValue = lastValue + rnd;
-      $scope.stimatedAmount = newValue;
-    }
-    // add the new value
-    $log.debug("realtimeGraphCtrl| pulse: " + pulse);
-    /*
-      MAGIC
-    */
-    if($scope.lastStrategyAmounts.length > 4){
-      let last_index = $scope.lastStrategyAmounts.length-1;
-      for(let index = 1; index<last_index; index++){
-        $scope.lastStrategyAmounts[index] = $scope.lastStrategyAmounts[index+1];
-        $scope.lastStrategyDates[index] = $scope.lastStrategyDates[index+1];
-      }
-      $scope.lastStrategyAmounts[last_index] = newValue;
-      $scope.lastStrategyDates[last_index] = now.getHoursFormatted() + ":" + now.getMinutesFormatted();
-    }else{
-      $scope.lastStrategyAmounts.push(newValue);
-      $scope.lastStrategyDates.push(now.getHoursFormatted() + ":" + now.getMinutesFormatted());
-    }
-    saveRealtime();
-    /*
-      /MAGIC
-    */
-
-  };
-
-  // check if exists somwthing in the local storage
+  // check if exists something in localStorage
   getRealtime();
-  if($scope.lastStrategyAmounts!=null){// exists
+  if(null == null){
+    // there isn't nothing
+    // get the data from last portfolio
+    let portfolios = strategyService.getLastStrategy().getPortfolios();
+    let portfolioNum = portfolios.length;
 
-  }else{// there is nothing in the local local storage
-
-    // get the history for the current strategy
-    $scope.strategyPortfolios = strategyService.getLastStrategy().getPortfolios();
-
-    let portfolioNum = $scope.strategyPortfolios.length;
-    // set the new array
-    $scope.lastStrategyDates = new Array(portfolioNum);
+    // initialize the array
     $scope.lastStrategyAmounts = new Array(portfolioNum);
+    $scope.lastStrategyDates = new Array(portfolioNum);
 
-    // set the date in human style
-    $scope.strategyPortfolios.forEach(function(portfolio,$index){
-      $scope.lastStrategyAmounts[$index] = portfolio.getTotalAmount();
+    // push inside amounts and dates
+    for(let i=0;i<portfolioNum;i++){
+      // th edate is a timestamp
+      $scope.lastStrategyDates[i]=portfolios[i].getDate().getTime();
+      // the amount is the total amount for each class assets
+      $scope.lastStrategyAmounts[i]=portfolios[i].getTotalAmount();
+    }
 
-      $scope.lastStrategyDates[$index] = portfolio.getFormattedDate();
-
-    });//for each end
+  }else{
+    // exists something
 
   }
-
 
 });
