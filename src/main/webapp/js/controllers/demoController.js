31d65506e1a2dc31fa06ba6408f59fc3a8e62cef
The_Bous <thebous1993@gmail.com>
2017-03-28 12:14:43 +0200

    demo calls

--- src/main/webapp/js/controllers/demoController.js
+++ src/main/webapp/js/controllers/demoController.js
@@ -1,11 +1,16 @@
 RoboAdviceApp.controller("demoController", function($scope, $log, demoREST, demoService){
+
     $log.debug("demo controller active");
+
     demoService.getDemoForecasting(function(response){
         if(response.statusCode == 0) {
-        $log.debug(response.data);
+            $log.debug(response.data);
         }
         else{
             $log.debug("demoController | statusCode = 1");
         }
     });
+
+
+
 });
