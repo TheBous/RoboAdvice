b60bafcf81e063c447a44fbfa7a83425608e85cd
Michele Falzone <michelefalzone@gmail.com>
2017-03-28 20:10:26 +0200

    demo backtesting fixed button callback, WIP: graph should refresh its data

--- src/main/webapp/js/controllers/demoController.js
+++ src/main/webapp/js/controllers/demoController.js
@@ -22,6 +22,35 @@ RoboAdviceApp.controller("demoController", function($scope, $log, demoREST, demo
         }
     });
 
+    $scope.backtestingCallback = function(response){
+      if(response.statusCode == 0){
+        $log.debug("backtesting calling ok");
+
+        $scope.spinner = false;
+        console.log(response);
+        for (let i = 0; i < response.data.length; i++) {
+            $scope.amount[i] = response.data[i].totalAmount;
+            $scope.date[i] = response.data[i].date.year + '/' + response.data[i].date.monthValue + '/' + response.data[i].date.dayOfMonth;
+            $scope.timestamp[i] = new Date($scope.date[i]).getTime();
+        }
+
+        $log.debug("backtesting response from the server:")
+        $log.debug($scope.amount);
+        $log.debug($scope.timestamp);
+
+      }else{
+        $log.error("backtesting calling error")
+      }
+    }
+
+
+    $scope.getAmount = function(){
+      return $scope.amount;
+    }
+    $scope.getTimestamp = function(){
+      return $scope.timestamp;
+    }
+
     $scope.update = function(param) {
         $scope.buttonClicked = false;
         let formattedDate;
@@ -31,18 +60,40 @@ RoboAdviceApp.controller("demoController", function($scope, $log, demoREST, demo
         $log.debug(formattedDate);
         $scope.spinner = true;
 
-        demoService.backtesting.bonds(formattedDate).$promise.then(function (response) {
-            $scope.spinner = false;
-            console.log(response);
-            for (let i = 0; i < response.data.length; i++) {
-                $scope.amount[i] = response.data[i].totalAmount;
-                $scope.date[i] = response.data[i].date.year + '/' + response.data[i].date.monthValue + '/' + response.data[i].date.dayOfMonth;
-                $scope.timestamp[i] = new Date($scope.date[i]).getTime();
-            }
-            $log.debug($scope.amount);
-            $log.debug($scope.timestamp);
-        });
-    }
+        switch(param){
+          case "Bonds":
+            $log.debug("backtesting on bonds strategy");
+            demoService.backtesting.bonds(formattedDate).$promise.then($scope.backtestingCallback);
+          break;
+          case "Income":
+            $log.debug("backtesting on income strategy");
+            demoService.backtesting.income(formattedDate).$promise.then($scope.backtestingCallback);
+          break;
+          case "Balanced":
+            $log.debug("backtesting on balanced strategy");
+            demoService.backtesting.balanced(formattedDate).$promise.then($scope.backtestingCallback);
+          break;
+          case "Growth":
+            $log.debug("backtesting on growth strategy");
+            demoService.backtesting.growth(formattedDate).$promise.then($scope.backtestingCallback);
+          break;
+          case "Stocks":
+            $log.debug("backtesting on stock strategy");
+            demoService.backtesting.stocks(formattedDate).$promise.then($scope.backtestingCallback);
+          break;
+          default:
+            demoService.backtesting.bonds(formattedDate).$promise.then($scope.backtestingCallback);
+            //
+            // let strategy = strategyService.getCurrentStrategy();
+            // $log.debug("backtesting on current strategy");
+            // $log.debug("fromdate: " + formattedDate);
+            // $log.debug("name:" + strategy.getName());
+            // $log.debug("percentage: " + strategy.getAssets());
+            // demoService.backtesting.custom(strategy.getName(),strategy.getAssets(),formattedDate);
+        }
+
+    }//end update
+
     $scope.updateOwn = function(param1, param2){
         let formattedDate;
         let date = new Date($scope.interval);
