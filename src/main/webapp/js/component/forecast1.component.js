3cec2e5576e482765b38789b7d8f163dd777d09b
The_Bous <thebous1993@gmail.com>
2017-03-29 18:53:59 +0200

    fix date in forecasting

--- src/main/webapp/js/component/forecast1.component.js
+++ src/main/webapp/js/component/forecast1.component.js
@@ -1,45 +1,46 @@
 RoboAdviceApp.component("forecastOne",{
-  bindings: {
-    startForecast: "&"
-  },
-  templateUrl: "../../html/forecastView-1.html",
+    bindings: {
+        startForecast: "&"
+    },
+    templateUrl: "../../html/forecastView-1.html",
 
-  controller: function($log){
-    let $ctrl = this;
-    this.$onInit=function(){
-        let now = new Date()
-        let maxMonth = new Date();
+    controller: function($log){
+        let $ctrl = this;
+        this.$onInit=function(){
+            let now = new Date();
+            now.setDate(now.getDate() + 1);
+            let maxMonth = new Date();
 
-        maxMonth.setMonth(maxMonth.getMonth()+3);
-        $('.datepicker').pickadate({
-          selectYears: 4, // Creates a dropdown of 14 years to control year
-            min: now,
-            max: maxMonth,
-        });
-        $('select').material_select();
-        this.info=function () {
-            swal({
-                title:"Expected trend!",
-                text:"The following prediction is based on Time Series, a model to generate predictions (forecasts) for future events based on known past chronological events.<br>It uses Weka Time Series library developed by Pentaho.",
-                html:true,
-                type: "warning"
+            maxMonth.setMonth(maxMonth.getMonth()+3);
+            $('.datepicker').pickadate({
+                selectYears: 4, // Creates a dropdown of 14 years to control year
+                min: now,
+                max: maxMonth,
             });
+            $('select').material_select();
+            this.info=function () {
+                swal({
+                    title:"Expected trend!",
+                    text:"The following prediction is based on Time Series, a model to generate predictions (forecasts) for future events based on known past chronological events.<br>It uses Weka Time Series library developed by Pentaho.",
+                    html:true,
+                    type: "warning"
+                });
+            }
         }
-    }
 
-    this.nextClick=function(){
-      $log.debug("nextClick clicked");
-      if($ctrl.forecastDate != null){
-        // default forecast
-        this.startForecast({date: $ctrl.forecastDate});
-      }else{
-        // forecast by date
-        let now = new Date();
-        
-        this.startForecast({date: null})
-      }
-    }
+        this.nextClick=function(){
+            $log.debug("nextClick clicked");
+            if($ctrl.forecastDate != null){
+                // default forecast
+                this.startForecast({date: $ctrl.forecastDate});
+            }else{
+                // forecast by date
+                let now = new Date();
 
-  }
+                this.startForecast({date: null})
+            }
+        }
+
+    }
 });
 
